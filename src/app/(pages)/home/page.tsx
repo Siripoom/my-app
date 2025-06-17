bb44a3547db35950ab5b63105252615f65ea250a
Siripoom <artorsiriratpoom@gmail.com>
2025-06-17 10:18:10 +0700

    update

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4 text-black">Welcome to My App</h1>
-      <p className="text-lg text-gray-700">This is a page.tsx</p>
+      <h1 className="text-4xl font-bold mb-4 text-black">Home</h1>
     </div>
   );
 }
---
+++ src/app/(pages)/team/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4 text-black">Team</h1>
+    </div>
+  );
+}
