86c24785578a7459230809b9a3af4f9aeac2c74c
Siripoom <artorsiriratpoom@gmail.com>
2025-07-05 10:48:49 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     "no-console": "off", // Example of disabling the 'no-console' rule
     "react/no-unescaped-entities": "off", // Another example of disabling a rule
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during production build
+  },
 };
 
 export default nextConfig;
