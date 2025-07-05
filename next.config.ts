b4cc7fe95fc274fd0eb876dd340476dba5e9b5b6
Siripoom <artorsiriratpoom@gmail.com>
2025-07-05 10:38:02 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rules: {
+    "no-console": "off", // Example of disabling the 'no-console' rule
+    "react/no-unescaped-entities": "off", // Another example of disabling a rule
+  },
 };
 
 export default nextConfig;
